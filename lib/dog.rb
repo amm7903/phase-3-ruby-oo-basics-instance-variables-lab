13f68ca5c12716ade4e1f3f6d055f1843dfaafdf
Alejandro Marcano <+>
2021-11-02 20:28:20 -0400

    commit tree 8d5fa7928131aa1dd9d602b51d309f83a8147c79

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
-class Dog
+# class Dog
+#     def name=(dog_name)
+#         this_dog_name = dog_name 
+#     end
 
-end
+#     def name 
+#        puts this_dog_name
+#     end
+# end
+
+# pogie = Dog.new
+# pogie.name = "pogie"
+
+class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+
+  lassie = Dog.new
+lassie.name = "Lassie"
